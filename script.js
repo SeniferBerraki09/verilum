4a1be1b2da6567a4ca76fbf67d8ed79436d1465e
SeniferBerraki09 <senifer.berraki@gmail.com>
2025-07-31 10:05:12 +0200

    version girls camp

---
+++ script.js
@@ -0,0 +1,21 @@
+// var prenom = prompt("Comment tu t'appelles?")
+
+// var titrePrincipal = document.getElementById("titre")
+
+// titrePrincipal.textContent="Bienvenue " + prenom
+
+// if(prenom== "Senifer") {
+// 	alert ("Comme moi !");
+// 	titrePrincipal.style.color = "yellow";
+// 	//pop-up + titre en vert
+// } else {
+// 	titrePrincipal.style.color= "red";	
+// 	//titre en rouge
+
+// }
+alert("LES AVATARS NOUS MENTENT!!!!!!!!")
+// alert("ILS SONT NEFASTES POUR NOTRE SOCIETE")
+
+const box = document.querySelector('.box');
+box.style.transition = 'transform 0.5s';
+box.style.transform = 'translateX(200px)';
